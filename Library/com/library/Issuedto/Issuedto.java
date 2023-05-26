e7d9bc2a05d64c26aceb61fd84232fa5ddf864e9
devangmittal <mittaldevang@gmail.com>
2023-05-27 00:19:55 +0530

    Add constructor and toString method

---
+++ Library/com/library/Issuedto/Issuedto.java
@@ -0,0 +1,20 @@
+package com.library.Issuedto;
+
+import com.library.Book.Book;
+import com.library.Member.Member;
+
+public class Issuedto {
+	Member m;
+	Book b;
+
+	Issuedto(Member m, Book b) {
+		this.m = m;
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return "Issuedto [Member = " + m.toString() + ", Book =" + b.toString() + "]";
+	}
+
+}
